56488c9d489bccbb62cfbccbe6dae8a1f55b0c80
vikas0330 <143322971+vikas0330@users.noreply.github.com>
2023-10-07 21:46:11 +0530

    Calculator

---
+++ Python/Make_and_use_Function.py
@@ -0,0 +1,22 @@
+def calculategmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+def isgreater(a,b):
+    if (a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater or equal")
+def ispower(a,b):
+    pow=a**b
+    print(pow)
+a=1
+b=3
+isgreater(a,b)
+calculategmean(a,b)
+ispower(a,b)
+c=5
+d=3
+isgreater(c,d)
+calculategmean (c,d)
+ispower(c,d)
+
