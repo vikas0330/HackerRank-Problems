25b85b5813ea085912db8ae6c95d2f08ebc8eed0
vikas0330 <143322971+vikas0330@users.noreply.github.com>
2023-10-11 15:32:38 +0530

    Alphabet Rangoli

--- Python/tempCodeRunnerFile.py
+++ Python/tempCodeRunnerFile.py
@@ -1,8 +1,3 @@
-map(int,input().split())
-# for i in range(n//2):
-#     j = int((2*i)+1)
-#     print(('.|.'*j).center(m, '-'))
-# print('WELCOME'.center(m,'-'))
-# for i in reversed(range(n//2)):
-#     j = int((2*i)+1)
-#     print(('.|.'*j).center
+
+#     n = int(input())
+#     print_formatted(n)
