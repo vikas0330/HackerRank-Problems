f6f599727ba603f2fcb302f6d8720d7a30aaa90a
vikas0330 <143322971+vikas0330@users.noreply.github.com>
2023-10-19 14:58:18 +0530

    Create Find_a_String.py

---
+++ Python/Find_a_String.py
@@ -0,0 +1,37 @@
+'''
+In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
+
+NOTE: String letters are case-sensitive.
+
+Input Format
+
+The first line of input contains the original string. The next line contains the substring.
+
+Constraints
+1<=len(string)<=200
+
+Each character in the string is an ascii character.
+
+Output Format
+
+Output the integer number indicating the total number of occurrences of the substring in the original string.
+'''
+
+def count_substring(string, sub_string):
+    c=0
+    l=len(sub_string)
+    for i  in range(len(string)):
+        s=string[i:i+l]
+        if s==sub_string:
+            c+=1
+    return c
+   
+    
+   
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
